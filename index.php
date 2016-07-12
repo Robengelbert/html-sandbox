2972adaee236a4cb315b246b5af2b0e151064541
Robert Engelbert <Robe32002@yahoo.com>
2016-07-12 12:52:43 -0600

    added paragraphs lists and links

--- index.php
+++ index.php
@@ -5,6 +5,20 @@
 		<title>My web page</title>
 	</head>
 	<body>
-		<h1>Hai World!</h1>
+		<h1>Fruit</h1>
+		<p>I love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesvI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesvvI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love apples</p>
+		<h2>Oranges</h2>
+		<p>Oranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangevvvOranges are orangeOranges are orangeOranges are orangeOranges are orangevvOranges are orange</p>
+		<h3>Click your favorite fruit to choose your travel destination</h3>
+		<ul>
+			<li><a href="http://www.experiencewa.com/">Apples</a></li>
+			<li><a href="http://www.visitflorida.com/en-us.html/">Oranges</a></li>
+			<li><a href="http://www.travelwisconsin.com/">Cherries</a></li>
+			<li><a href="http://www.visitmexico.com/">Bananas</a></li>
+		</ul>
+		<a href="https://bootcamp-coders.cnm.edu/" target="_blank">Click me</a>
+		<div class="wrapper">
+			<p>I can use this div as a wrapper to style it and stuff</p>
+		</div>
 	</body>
 </html>
