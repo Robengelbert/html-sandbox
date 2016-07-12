174aceec15184e83900a3358d8e3fa40186f5c8e
Robert Engelbert <Robe32002@yahoo.com>
2016-07-12 15:08:42 -0600

    added comments

--- index.php
+++ index.php
@@ -5,22 +5,27 @@
 		<title>My web page</title>
 	</head>
 	<body>
+		<!-- this is a header with an h1 that say's fruit -->
 		<header>
 			<h1>Fruit</h1>
 		</header>
+		<!-- start main content here -->
 		<main>
 		<p>I love applesI love applesI love apples<strong>I love apples</strong>I love applesI love applesI love applesI love applesI love applesI love applesvI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesvvI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love apples</p>
+			<!-- this is an image -->
 		<img src="images/fun.jpe" alt="People having fun."/>
 		<h2>Oranges</h2>
 		<p>Oranges are orange<em>Oranges are orange</em>Oranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangevvvOranges are orangeOranges are orangeOranges are orangeOranges are orangevvOranges are orange</p>
 		<h3>Click your favorite fruit to choose your travel destination</h3>
+			<!-- this is navigation -->
 		<ul>
-			<li><a href="http://www.experiencewa.com/">Apples</a></li>
-			<li><a href="http://www.visitflorida.com/en-us.html/">Oranges</a></li>
-			<li><a href="http://www.travelwisconsin.com/">Cherries</a></li>
-			<li><a href="http://www.visitmexico.com/">Bananas</a></li>
+			<li><a href="http://www.experiencewa.com/" target="_blank">Apples</a></li>
+			<li><a href="http://www.visitflorida.com/en-us.html/" target="_blank">Oranges</a></li>
+			<li><a href="http://www.travelwisconsin.com/" target="_blank">Cherries</a></li>
+			<li><a href="http://www.visitmexico.com/" target="_blank">Bananas</a></li>
 		</ul>
 		<a href="https://bootcamp-coders.cnm.edu/" target="_blank">Click me</a>
+			<!-- this is a div-->
 		<div class="wrapper">
 			<p>I can use this div as a wrapper to style it and stuff</p>
 		</div>
@@ -43,6 +48,8 @@
 			</tr>
 		</table>
 		</main>
+		<!-- End of main content -->
+		<!-- copyright information -->
 		<footer>
 			&copy; 2016 rengelbert
 		</footer>
