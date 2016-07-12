834947497f7793e8592d52c4dad0e88ebcfb940b
Robert Engelbert <Robe32002@yahoo.com>
2016-07-12 14:56:55 -0600

    added paragraphs lists and links and content

--- index.php
+++ index.php
@@ -5,10 +5,14 @@
 		<title>My web page</title>
 	</head>
 	<body>
-		<h1>Fruit</h1>
-		<p>I love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesvI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesvvI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love apples</p>
+		<header>
+			<h1>Fruit</h1>
+		</header>
+		<main>
+		<p>I love applesI love applesI love apples<strong>I love apples</strong>I love applesI love applesI love applesI love applesI love applesI love applesvI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesvvI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love applesI love apples</p>
+		<img src="images/fun.jpe" alt="People having fun."/>
 		<h2>Oranges</h2>
-		<p>Oranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangevvvOranges are orangeOranges are orangeOranges are orangeOranges are orangevvOranges are orange</p>
+		<p>Oranges are orange<em>Oranges are orange</em>Oranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangeOranges are orangevvvOranges are orangeOranges are orangeOranges are orangeOranges are orangevvOranges are orange</p>
 		<h3>Click your favorite fruit to choose your travel destination</h3>
 		<ul>
 			<li><a href="http://www.experiencewa.com/">Apples</a></li>
@@ -20,5 +24,27 @@
 		<div class="wrapper">
 			<p>I can use this div as a wrapper to style it and stuff</p>
 		</div>
+		<table>
+			<tr>
+				<th>
+					cell 1
+				</th>
+				<th>
+					cell 2
+				</th>
+			</tr>
+			<tr>
+				<td>
+					cell 3
+				</td>
+				<td>
+					cell 4
+				</td>
+			</tr>
+		</table>
+		</main>
+		<footer>
+			&copy; 2016 rengelbert
+		</footer>
 	</body>
 </html>
