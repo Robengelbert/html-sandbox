6da03458f2e7ea06c867e09aa0efcb3ceeff8366
Robert Engelbert <Robe32002@yahoo.com>
2016-08-04 11:47:09 -0600

    Playing with bootstrap in class.

---
+++ portfolio.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+		<meta http-equiv="x-ua-compatible" content="ie=edge" />
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+		
+		<link href="css/main.css" rel="stylesheet" type="text/css" />
+
+		<!-- jQuery first, then Bootstrap JS. -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<title>My bootstrap page</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						</div>
+					</div>
+				</div>
+		</header>
+		<!-- begin main content -->
+		<main>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3">
+						side panel
+					</div>
+					<div class="col-md-9">
+						content area
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-4">
+						column 1
+					</div>
+					<div class="col-md-4">
+						column 2
+					</div>
+					<div class="col-md-4">
+						column 3
+					</div>
+				</div>
+			</div>
+		</main>
+		</div>
+		<!-- begin footer -->
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6">
+						footer column one
+					</div>
+					<div class="col-md-6">
+						footer column two
+					</div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
