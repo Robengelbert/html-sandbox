814b7f52005e87c0665a9f01045f5a7d51fca8ba
Robert Engelbert <Robe32002@yahoo.com>
2016-08-26 14:17:27 -0600

    Added a jquery practice directory and files

---
+++ jquery/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+		<script src="script.js" type="text/javascript"></script>
+		<title>JQuery practice</title>
+	</head>
+	<body>
+		<p>Dogs Rule</p>
+		<p class="mark">Dogs Rule</p>
+		<p>Dogs Rule</p>
+		<button type="button">replace text</button>
+
+	</body>
+</html>
---
+++ jquery/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$("button").click(function(){
+		$("p").text("I'm a lover");
+		$("p").css("background", "hotpink");
+	});
+});
