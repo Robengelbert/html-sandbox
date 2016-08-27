7cf38133375c15b79004208c5ffa922acf743dbc
Robert Engelbert <Robe32002@yahoo.com>
2016-08-26 19:47:46 -0600

    Added some different functions to practice with jquery

--- jquery/index.php
+++ jquery/index.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
-	<html lang="en">
+<html lang="en">
 	<head>
-		<meta charset="utf-8" />
+		<meta charset="utf-8"/>
 		<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
 		<script src="script.js" type="text/javascript"></script>
+		<link href="style.css" type="text/css" rel="stylesheet"/>
 		<title>JQuery practice</title>
 	</head>
 	<body>
-		<p>Dogs Rule</p>
-		<p class="mark">Dogs Rule</p>
-		<p>Dogs Rule</p>
-		<button type="button">replace text</button>
+		<h1>JQuery test site</h1>
+		<p>I'm happy that I'm learning so much here, it took me a long time to get to this point.</p>
+		<p class="mark">I hope to be able to find work quickly after I get out of school.</p>
+		<p>I'm sure I'll build some free sites for people just to get some experience.</p>
+
+		<form>
+					<input type="text" id="inputMobile" maxlength="10" placeholder="Enter a mobile number">
+
+					<input type="submit" value="submit">
+			<p></p>
+					<button type="button">replace text</button>
+
+		</form>
 
 	</body>
 </html>
--- jquery/script.js
+++ jquery/script.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
-	$("p").hover(function() {
-		$(this).addClass("highlight");
-	}, function(){
-		$($this).removeClass("highlight");
+	$("form").submit(function(event){
+		var mob = /^[1-9]{1}[0-9]{9}$/;
+		var currentValue = $("#inputMobile").val();
+		if(mob.test(currentValue) == false && currentValue != 10){
+			$("p").html("Invalid mobile number").addClass("error").show().fadeOut(1000);
+		}else{
+			$("p").html("Valid mobile number").addClass("success").show().fadeOut(1000);
+		}
+		event.preventDefault();
+
 	});
 });
